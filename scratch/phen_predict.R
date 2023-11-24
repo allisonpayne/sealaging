788b28018e14b220ec9602d97df64d97182b053f
Allison Payne <allisonpayne01@gmail.com>
2023-11-24 11:51:31 -0800

    Fixing axis font sizes

---
+++ scratch/phen_predict.R
@@ -0,0 +1,23 @@
+
+expand_grid(
+  age = 9:19,
+  observed = c("Breeder", "Non-breeder")
+) %>% 
+  mutate(ba = predict(phenology_models$model[[1]],
+                      newdata = .,
+                      re.form = NA),
+         bd = predict(phenology_models$model[[2]],
+                      newdata = .,
+                      re.form = NA), 
+         ma = predict(phenology_models$model[[3]],
+                      newdata = .,
+                      re.form = NA), 
+         md = predict(phenology_models$model[[4]],
+                      newdata = .,
+                      re.form = NA)) %>% 
+  mutate(short_trip = ma - bd, 
+         molt_dur = md - ma) %>% 
+  filter(observed == "Breeder", 
+         age %in% c(9,19)) %>% 
+  view()
+
